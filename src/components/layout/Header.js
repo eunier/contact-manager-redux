dca3faa284b800a86a7fa14cd677f77b0843a761
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-23 19:06:32 -0400

    Fix bug with header brand

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = props => {
   const { branding } = props;
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <NavLink exact to="/" className="navbar-brand">
           {branding}
-        </a>
+        </NavLink>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="/" className="nav-link">
-                <i className="fas fa-home" /> Home
-              </Link>
+              <NavLink exact to="/" className="nav-link">
+                <i className="fas fa-home"> Home</i>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/contact/add" className="nav-link">
-                <i className="fas fa-plus" /> Add
-              </Link>
+              <NavLink exact to="/contact/add" className="nav-link">
+                <i className="fas fa-plus"> Add</i>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-link">
-                <i className="fas fa-question" /> About
-              </Link>
+              <NavLink exact to="/about" className="nav-link">
+                <i className="fas fa-question"> About</i>
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -38,7 +39,7 @@ Header.defaultProps = {
   branding: 'My App'
 };
 
-Header.propTypes = {
+Header.prototype = {
   branding: PropTypes.string.isRequired
 };
 
