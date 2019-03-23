5057591e4caf4fa6d0a9c81579ed688f6f1879ed
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-23 17:42:31 -0400

    Add PUT request and dispatch

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getContact } from '../../actions/contactActions';
+import { getContact, updateContact } from '../../actions/contactActions';
 
 class EditContact extends Component {
   state = {
@@ -43,13 +43,16 @@ class EditContact extends Component {
       return;
     }
 
+    const { id } = this.props.match.params;
+
     const updContact = {
+      id,
       name,
       email,
       phone
     };
 
-    const { id } = this.props.match.params;
+    this.props.updateContact(updContact);
 
     //// UPDATE CONTACT ////
 
@@ -122,5 +125,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getContact }
+  { getContact, updateContact }
 )(EditContact);
